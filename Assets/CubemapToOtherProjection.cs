b444e213508a65e0463c31e38a9ebf3a16e1e051
aosoft <tan-y@aosoft.jp>
2018-04-10 01:44:18 +0900

    Updated CubemapToOtherProjection
    * Support Equirectanglur (360 or 180)
    * Support FishEye (multi type)

--- Assets/CubemapToOtherProjection.cs
+++ Assets/CubemapToOtherProjection.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum StereoType
+{
+	None,
+	LeftRight,
+	TopDown
+}
+
+public enum ProjectionType
+{
+	Equirectangular_360 = 0,
+	Equirectangular_180,
+	FishEye_Equidistance,
+	FishEye_EquisolidAngle,
+	FishEye_Orthogonal
+}
+
 [RequireComponent(typeof(Camera))]
 public class CubemapToOtherProjection : MonoBehaviour
 {
 	public RenderTexture RenderTarget;
 	public int CubemapSize = 1024;
+	public ProjectionType ProjectionType = ProjectionType.Equirectangular_360;
 	public bool RenderInStereo = false;
 	public float StereoSeparation = 0.065f;
+
 
 	private Camera _camera;
 	private Material _material;
@@ -34,7 +52,57 @@ public class CubemapToOtherProjection : MonoBehaviour
 
 		var matrix = Matrix4x4.Rotate(_camera.transform.localRotation);
 		_material.SetMatrix("_Matrix", matrix);
-		_material.EnableKeyword("ANGLEFUNC_EQUISOLIDANGLE");
+
+		switch (ProjectionType)
+		{
+			case ProjectionType.Equirectangular_360:
+				{
+					SetUVScaleOffset(Mathf.PI * 2.0f, Mathf.PI, 0.0f, 0.0f); 
+					SetEnableProjFishEye(false);
+					SetEnableEquiSolidAngle(false);
+					SetEnableOrthogal(false);
+					SetEquirectangularScale(2.0f);
+				}
+				break;
+
+			case ProjectionType.Equirectangular_180:
+				{
+					SetUVScaleOffset(Mathf.PI, Mathf.PI, Mathf.PI * 0.5f, 0.0f);
+					SetEnableProjFishEye(false);
+					SetEnableEquiSolidAngle(false);
+					SetEnableOrthogal(false);
+					SetEquirectangularScale(1.0f);
+				}
+				break;
+
+			case ProjectionType.FishEye_Equidistance:
+				{
+					SetUVScaleOffset(2.0f, 2.0f, -1.0f, -1.0f);
+					SetEnableProjFishEye(true);
+					SetEnableEquiSolidAngle(false);
+					SetEnableOrthogal(false);
+				}
+				break;
+
+			case ProjectionType.FishEye_EquisolidAngle:
+				{
+					SetUVScaleOffset(2.0f, 2.0f, -1.0f, -1.0f);
+					SetEnableProjFishEye(true);
+					SetEnableEquiSolidAngle(true);
+					SetEnableOrthogal(false);
+				}
+				break;
+
+			case ProjectionType.FishEye_Orthogonal:
+				{
+					SetUVScaleOffset(2.0f, 2.0f, -1.0f, -1.0f);
+					SetEnableProjFishEye(true);
+					SetEnableEquiSolidAngle(false);
+					SetEnableOrthogal(true);
+				}
+				break;
+		}
+
 
 		if (RenderInStereo)
 		{
@@ -43,13 +111,27 @@ public class CubemapToOtherProjection : MonoBehaviour
 			_camera.stereoSeparation = StereoSeparation;
 			_camera.stereoTargetEye = StereoTargetEyeMask.Both;
 
-			_camera.RenderToCubemap(_cubemap, 63, Camera.MonoOrStereoscopicEye.Left);
-			SetViewport(new Vector2(0.5f, 1.0f), new Vector2(-0.5f, 0.0f));
-			Graphics.Blit(_cubemap, RenderTarget, _material);
+			if (ProjectionType == ProjectionType.Equirectangular_360)
+			{
+				_camera.RenderToCubemap(_cubemap, 63, Camera.MonoOrStereoscopicEye.Left);
+				SetPositionScaleOffset(1.0f, 0.5f, 0.0f, -0.5f);
+				Graphics.Blit(_cubemap, RenderTarget, _material);
 
-			_camera.RenderToCubemap(_cubemap, 63, Camera.MonoOrStereoscopicEye.Right);
-			SetViewport(new Vector2(0.5f, 1.0f), new Vector2(0.5f, 0.0f));
-			Graphics.Blit(_cubemap, RenderTarget, _material);
+				_camera.RenderToCubemap(_cubemap, 63, Camera.MonoOrStereoscopicEye.Right);
+				SetPositionScaleOffset(1.0f, 0.5f, 0.0f, 0.5f);
+				Graphics.Blit(_cubemap, RenderTarget, _material);
+			}
+			else
+			{
+				_camera.RenderToCubemap(_cubemap, 63, Camera.MonoOrStereoscopicEye.Left);
+				SetPositionScaleOffset(0.5f, 1.0f, -0.5f, 0.0f);
+				Graphics.Blit(_cubemap, RenderTarget, _material);
+
+				_camera.RenderToCubemap(_cubemap, 63, Camera.MonoOrStereoscopicEye.Right);
+				SetPositionScaleOffset(0.5f, 1.0f, 0.5f, 0.0f);
+				Graphics.Blit(_cubemap, RenderTarget, _material);
+			}
+
 
 			_camera.stereoSeparation = tmpStereoSepration;
 			_camera.stereoTargetEye = tmpStereoTargetEye;
@@ -57,7 +139,7 @@ public class CubemapToOtherProjection : MonoBehaviour
 		else
 		{
 			_camera.RenderToCubemap(_cubemap);
-			SetViewport(new Vector2(1.0f, 1.0f), new Vector2(0.0f, 0.0f));
+			SetPositionScaleOffset(1.0f, 1.0f, 0.0f, 0.0f);
 			Graphics.Blit(_cubemap, RenderTarget, _material);
 		}
 	}
@@ -77,20 +159,45 @@ public class CubemapToOtherProjection : MonoBehaviour
 		}
 	}
 
-	private void SetViewport(Vector2 scale, Vector2 offset)
+	private void SetEnableProjFishEye(bool flag)
 	{
-		_material.SetVector("_ScaleOffset", new Vector4(scale.x, scale.y, offset.x, offset.y));
+		SetEnableKeyword("PROJ_FISHEYE", flag);
 	}
 
-	private void SetEnableKeyword(Material material, string keyword, bool flag)
+	private void SetEnableEquiSolidAngle(bool flag)
+	{
+		SetEnableKeyword("ANGLEFUNC_EQUISOLIDANGLE", flag);
+	}
+
+	private void SetEnableOrthogal(bool flag)
+	{
+		SetEnableKeyword("ANGLEFUNC_ORTHGONAL", flag);
+	}
+
+	private void SetPositionScaleOffset(float scaleX, float scaleY, float offsetX, float offsetY)
+	{
+		_material.SetVector("_PositionScaleOffset", new Vector4(scaleX, scaleY, offsetX, offsetY));
+	}
+
+	private void SetUVScaleOffset(float scaleX, float scaleY, float offsetX, float offsetY)
+	{
+		_material.SetVector("_UVScaleOffset", new Vector4(scaleX, scaleY, offsetX, offsetY));
+	}
+
+	private void SetEquirectangularScale(float scale)
+	{
+		_material.SetFloat("_EquirectangularScale", scale);
+	}
+
+	private void SetEnableKeyword(string keyword, bool flag)
 	{
 		if (flag)
 		{
-			material.EnableKeyword(keyword);
+			_material.EnableKeyword(keyword);
 		}
 		else
 		{
-			material.DisableKeyword(keyword);
+			_material.DisableKeyword(keyword);
 		}
 	}
 }
--- Assets/CubemapToOtherProjection.shader
+++ Assets/CubemapToOtherProjection.shader
@@ -14,12 +14,14 @@ Shader "Conversion/CubemapToOtherProjection" {
 				#pragma vertex vert
 				#pragma fragment frag
 				#pragma fragmentoption ARB_precision_hint_fastest
-				#pragma multi_compile _ ANGLEFUNC_EQUISOLIDANGLE ANGLEFUNC_ORTHOGRAPHIC
+				#pragma multi_compile _ PROJ_FISHEYE 
+				#pragma multi_compile _ ANGLEFUNC_EQUISOLIDANGLE ANGLEFUNC_ORTHGONAL
 				//#pragma fragmentoption ARB_precision_hint_nicest
 				#include "UnityCG.cginc"
 
-				#define PI    3.141592653589793
-				#define TWOPI 6.283185307179587
+				#define PI     3.141592653589793
+				#define TWOPI  6.283185307179587
+				#define PIDIV2 1.570796326794897
 
 				struct v2f {
 					float4 pos : POSITION;
@@ -28,40 +30,52 @@ Shader "Conversion/CubemapToOtherProjection" {
 		
 				samplerCUBE _MainTex;
 				uniform float4x4 _Matrix;
-				uniform float4 _ScaleOffset;
+				uniform float4 _PositionScaleOffset;
+				uniform float4 _UVScaleOffset;
 
 				v2f vert( appdata_img v )
 				{
 					v2f o;
 					o.pos = UnityObjectToClipPos(v.vertex);
-					o.pos.xy = o.pos * _ScaleOffset.xy + _ScaleOffset.zw;
-					o.uv = (v.texcoord.xy - float2(0.5, 0.5)) * float2(2.0, 2.0);
+					o.pos.xy = o.pos * _PositionScaleOffset.xy + _PositionScaleOffset.zw;
+					o.uv = v.texcoord.xy * _UVScaleOffset.xy + _UVScaleOffset.zw;
+
 					return o;
 				}
 		
 				fixed4 frag(v2f i) : COLOR 
 				{
+					float4 unit = float4(0, 0, 0, 1);
+
+#if PROJ_FISHEYE
+
 					float len = length(i.uv);
 					if (len > 1.0)
 					{
 						return fixed4(0.0, 0.0, 0.0, 1.0);
 					}
 
-#if defined(ANGLEFUNC_EQUISOLIDANGLE)
+#if ANGLEFUNC_EQUISOLIDANGLE
 					float ang_yz = 2.0 * asin(len / 2.0);
-#elif defined(ANGLEFUNC_ORTHOGRAPHIC)
+#elif ANGLEFUNC_ORTHGONAL
 					float ang_yz = asin(len);
 #else
-					float ang_yz = len * PI/2;
+					float ang_yz = len * PIDIV2;
 #endif
-					float4 unit = float4(0, 0, 0, 1);
 
 					float ang_yx = atan2(i.uv.y, i.uv.x);
 					float sin_ang_yz = sin(ang_yz);
 					unit.x = cos(ang_yx) * sin_ang_yz;
 					unit.y = sin(ang_yx) * sin_ang_yz;
 					unit.z = cos(ang_yz);
+#else
+					float theta = i.uv.y;
+					float phi = i.uv.x;
 
+					unit.x = sin(phi) * sin(theta) * -1;
+					unit.y = cos(theta) * -1;
+					unit.z = cos(phi) * sin(theta) * -1;
+#endif
 					return texCUBE(_MainTex, mul(_Matrix, unit).xyz);
 				}
 			ENDCG
